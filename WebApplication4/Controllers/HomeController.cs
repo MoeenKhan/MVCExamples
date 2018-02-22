f37760749f9f2b0e3b815e94411056c4c92b6cf7
MoeenKhan <moeen.khan021@gmail.com>
2018-02-22 05:35:00 +0500

    MVCExamples

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Code1()
+        {
+            ViewData["message"] = "ViewData message here";
+            ViewData["currenttime"] = DateTime.Now;
+            return View();
+
+        }
+
+        public ActionResult Code2()
+        {
+            ViewBag.message = "ViewBag message here";
+            ViewBag.currenttime = DateTime.Now;
+            return View();
+        }
+
+        public ActionResult Code3()
+        {
+            ViewBag.something = "message here";
+            return View();
+
+        }
+
+        public ActionResult Code4()
+        {
+            TempData["tempmsg"] = "Temp Message";
+            return Redirect("Code4b");
+        }
+        
+        public ActionResult Code4b()
+        {
+            return View();
+
+        }
+
+        public ActionResult Code5()
+        {
+            return View();
+        }
+
+        public ActionResult Code6()
+        {
+            return View();
+        }
+
+        public ActionResult Code7()
+        {
+            return View();
+        }
+
+        public ActionResult Code8()
+        {
+            return View();
+        }
+
+        public ActionResult Code9()
+        {
+            return View();
+        }
+
+        public ActionResult Code10()
+        {
+            return View();
+        }
+
+        public ActionResult Code11()
+        {
+            return View();
+        }
+	}
+}
